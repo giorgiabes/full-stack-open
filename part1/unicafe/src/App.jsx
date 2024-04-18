8976f902ab9a496cc3162d213d4e02d763cb5e14
giorgiabes <abesadze.george@gmail.com>
2024-04-18 22:42:00 +0400

    unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+const Head = () => <h1>give feedback</h1>;
+
+const Button = ({ increment, text }) => {
+  return <button onClick={increment}>{text}</button>;
+};
+
+const Statistics = () => <h1>statistics</h1>;
+
+const Display = ({ text, score }) => (
+  <div>
+    {text} {score}
+  </div>
+);
+
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrGood = () => setGood(good + 1);
+  const incrNeutral = () => setNeutral(neutral + 1);
+  const incrBad = () => setBad(bad + 1);
+
   return (
     <div>
-      <div>changeme</div>
+      <Head />
+      <Button increment={incrGood} text="good" />
+      <Button increment={incrNeutral} text="neutral" />
+      <Button increment={incrBad} text="bad" />
+      <Statistics />
+      <Display text="good" score={good} />
+      <Display text="neutral" score={neutral} />
+      <Display text="bad" score={bad} />
     </div>
   );
 }
