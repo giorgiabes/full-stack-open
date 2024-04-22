8809e64993cae88ee402f2aa5484c7a9b271a06f
giorgiabes <abesadze.george@gmail.com>
2024-04-22 14:55:41 +0400

    anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,44 @@
 import { useState } from "react";
 
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Display = ({ anecdotes, selected, points }) => {
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+    </>
+  );
+};
+
+const MostVoted = ({ anecdotes, points }) => {
+  function indexOfMax(arr) {
+    let max = Math.max(...arr);
+    return arr.indexOf(max);
+  }
+
+  if (points.reduce((acc, curr) => acc + curr, 0) === 0) {
+    return (
+      <>
+        <h1>Anecdote with most votes</h1>
+        <p>
+          To display most voted anecdote, there should be at last one voted
+          anecdote
+        </p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[indexOfMax(points)]}</div>
+    </>
+  );
+};
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -13,6 +52,7 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [points, setPoint] = useState(new Uint8Array(anecdotes.length));
 
   const getRandomInt = (min, max) => {
     min = Math.ceil(min);
@@ -22,10 +62,18 @@ const App = () => {
 
   const handleNext = () => setSelected(getRandomInt(0, anecdotes.length));
 
+  const handleVote = () => {
+    const newSelected = [...points];
+    newSelected[selected] += 1;
+    return setPoint(newSelected);
+  };
+
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
-      <button onClick={handleNext}>next anecdote</button>
+      <Display anecdotes={anecdotes} selected={selected} points={points} />
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleNext} text="next anecdote" />
+      <MostVoted anecdotes={anecdotes} points={points} />
     </div>
   );
 };
